b8868c915d2c57f3fc65c115b73629f0af8cd2e7
kalmaske <kalmaske@uncc.edu>
2017-10-04 14:14:26 -0400

    ..

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const postsController = require("../../controllers/postsController.js");
+const db = require("./models");
 
 module.exports = {
    
--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
+
+const db = require("./models");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+//using api routes
+app.use(routes);
+
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(`🌎 ==> Server now on port ${PORT}!`);
+  });
 });
