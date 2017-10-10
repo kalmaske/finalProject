8ef3624dd6275e2669a24b87c0c68c095853d4ef
Tiara Hamilton <tnhami63@gmail.com>
2017-10-10 12:43:07 -0400

    update11

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -18,41 +18,46 @@ class Home extends Component {
     const { isAuthenticated } = this.props.auth;
 
     return (
-    <div>
-      <div className="container">
-        <div className="jumbotron jumbotron-fluid">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-8">
-              <h1 className="display-3">Aggrandize</h1>
-                <hr />
-              <br />
-              <p className="lead">(v.) increasing the power, greatness, and wealth of</p>
-              </div>
-              <div className="col-md-4">
-                <img src="./assets/images/favicon.png" alt="creative brain" />
-              </div>
-              </div>
+      <div><div className="container">
+      <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-7" style="font-family: 'Permanent Marker', cursive;">
+            <center><h1 className="display-3">Aggrandize</h1>
+              <hr />
+            <br />
+            <p className="lead">(v.) increasing the power, greatness, and wealth of</p></center>
             </div>
-        </div>
-      </div>
-  
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <center><h2>About the Site</h2></center>
-            <p>At its crux, Name of App is a beacon for those suffering from depression and mental illness. 
-            The application gives freedom of expression to users in need of liberating their innermost turmoil in a multitude of platforms. 
-            The app allows users to showcase poetry, short stories, photography, originally composed music, mini films, and video through 
-            social media and an aggregate website.</p>
-                
-            <p>The application gives those voice who would otherwise stifle their muse and remain disjointed from the staggering population 
-            languishing from mental illness. The ingenuity of the application stems from its ability to platform creative expression, encourage 
-            and support connection among users, raises awareness of mental illness, and boons a healthy means to tackle pernicious emotions.  
-            </p> 
+            <div className="col-md-5">
+              <img id="jumPic" src="./assets/images/favicon1.png" alt="creative brain" />
+            </div>
+            </div>
           </div>
-          <div className="col-md-6">
-           <center>
+      </div>
+    </div>
+
+
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6" >
+            <div style="font-family: 'Permanent Marker', cursive;">  
+          <center><h2>About the Site</h2></center></div>
+          <div className="bubble"><p>At its crux, Name of App is a beacon for those suffering from depression and mental illness. 
+          The application gives freedom of expression to users in need of liberating their innermost turmoil in a multitude of platforms. 
+          The app allows users to showcase poetry, short stories, photography, originally composed music, mini films, and video through 
+          social media and an aggregate website.</p>
+              
+          <p>The application gives those voice who would otherwise stifle their muse and remain disjointed from the staggering population 
+          languishing from mental illness. The ingenuity of the application stems from its ability to platform creative expression, encourage 
+          and support connection among users, raises awareness of mental illness, and boons a healthy means to tackle pernicious emotions.  
+          </p>
+          </div>
+          <br />
+        </div>
+        <div className="col-md-6">
+            <center><div style="font-family: 'Permanent Marker', cursive;"> 
+         <h2>Want to Join In?</h2></div></center>
+         <center>
            {
               !isAuthenticated() && (
                 <div>
@@ -90,10 +95,9 @@ class Home extends Component {
                 )
             } 
             </center>
-          </div>
         </div>
       </div>
-      </div>
+    </div></div>
     );
   }
 }
